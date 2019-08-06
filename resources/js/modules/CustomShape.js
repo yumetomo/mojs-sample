c180580365c670a643f3c031e3b75440cbc7a6ca
Yumetomo <yumetomo.eto@imjp.co.jp>
2019-08-06 18:33:49 +0900

    [update] カスタムシェイプの長さ調整

--- resources/js/modules/CustomShape.js
+++ resources/js/modules/CustomShape.js
@@ -1,7 +1,7 @@
 export default function () {
   class Heart extends mojs.CustomShape {
     getShape() { return '<path d="M92.6 7.4c-10-9.9-26-9.9-35.9 0l-4.4 4.3a3.4 3.4 0 0 1-4.7 0l-4.3-4.3c-10-9.9-26-9.9-35.9 0a25 25 0 0 0 0 35.5l22.4 22.2 13.5 13.4a9.5 9.5 0 0 0 13.4 0L70.2 65 92.6 43a25 25 0 0 0 0-35.5z"/>'; }
-    getLength() { return 292.110107421875; }
+    getLength() { return 292.1283874511719; }
   }
   mojs.addShape('heart', Heart);
 
@@ -25,9 +25,12 @@ export default function () {
     fill: 'none',
     stroke: 'deeppink',
     strokeDasharray: '100%',
-    strokeDashoffset: { '-100%': '100%' },
+    strokeDashoffset: { '-100%': '0%' },
     left: '75%',
-    duration: 2000,
+    duration: 1000
+  }).then({
+    strokeDashoffset: { '0%': '100%' },
+    duration: 1000,
     onComplete() {
       this.replay();
     }
